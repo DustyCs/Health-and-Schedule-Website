633f9599af9339734882f2a045211e1931cfe34f
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-02-02 16:11:15 +0800

    Testing for frontend routing

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div>
-      <div className="Container bg-red-500">
-        <h1>Hello World</h1>
+    <>
+      <div>
+        <div className="Container bg-red-500">
+          <h1>Supposed Nav</h1>
+        </div>
       </div>
-    </div>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>  
+      </Router>
+    </>
   );
 }
 
